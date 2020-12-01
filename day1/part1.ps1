0c2a750a5c1ec6e348a98183e51c2bf2bddc7287
Pete Mallam <pete.mallam@bjss.com>
2020-12-01 14:07:35 +0000

    Full commit

--- day1/part1.ps1
+++ day1/part1.ps1
@@ -1,10 +1,13 @@
-# $values from advent list
+﻿# AdventOfCode 2020 - Day 1 Part 1
+$values = get-content .\values.txt
 $values=$values.split("`n`r")
 
 foreach ( $passone in $values ) {
- foreach ( $passtwo in $values ) {
-  if ( [int]$passone + [int]$passtwo -eq 2020 ) {
-    write-host $passone
-  }
- }
+    foreach ( $passtwo in $values ) {
+        if ( $passone -gt 0 -and $( [int]$passone + [int]$passtwo ) -eq 2020 ) {
+            write-host "$passone + $passtwo = $( [int]$passone + [int]$passtwo )";
+            write-host "$passone x $passtwo = $( [int]$passone * [int]$passtwo )"
+            exit;
+        }
+    }
 }
---
+++ day1/part2.ps1
@@ -0,0 +1,15 @@
+﻿# AdventOfCode 2020 - Day 1 Part 2
+$values = get-content .\values.txt
+$values=$values.split("`n`r")
+
+foreach ( $passone in $values ) {
+    foreach ( $passtwo in $values ) {
+        foreach ( $passthree in $values ) {
+            if ( $passone -gt 0 -and $( [int]$passone + [int]$passtwo + [int]$passthree ) -eq 2020 ) {
+                write-host "$passone + $passtwo + $passthree = $( [int]$passone + [int]$passtwo + [int]$passthree )"
+                write-host "$passone x $passtwo x $passthree = $( [int]$passone * [int]$passtwo * [int]$passthree )"
+                exit;
+            }
+        }
+    }
+}
